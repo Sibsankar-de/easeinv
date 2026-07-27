0ffffacb46c241e09516fe929fdf24f5ec01086d
Sibsankar-de <sibsankarde910@gmail.com>
2026-07-28 01:44:29 +0530

    fix: Zod depricateed validator updated.

--- server/src/utils/validate.utils.ts
+++ server/src/utils/validate.utils.ts
@@ -1,11 +1,10 @@
-import { Schema } from "zod";
+import { ZodType } from "zod";
 import { ApiError } from "./apiErrorHandler";
 import { StatusCodes } from "http-status-codes";
 
-export const validateBody = <T>(schema: Schema<T>, data: unknown): T => {
+export const validateBody = <T>(schema: ZodType<T>, data: unknown): T => {
   const result = schema.safeParse(data);
   if (!result.success) {
-    // Extract first error message and throw a BAD_REQUEST ApiError
     const firstError = result.error.issues[0];
     const errorMessage = firstError.message;
     throw new ApiError(StatusCodes.BAD_REQUEST, errorMessage);
